f74e58491ab72808424908db96e27a53eb74a5fd
sonorks <julivas96@gmail.com>
2018-05-05 04:53:43 -0500

    Location

--- homes/views.py
+++ homes/views.py
@@ -2,13 +2,16 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 
-from .models import Homes, Agency
+from .models import Homes, Agency, Location
 
 # Create your views here.
 def getAllHomes(request):
     agency = Agency.objects.values('name','nit')
     homes = Homes.objects.all().values()
     homes_list = list(homes)
+    for home in homes:
+        location = Location.objects.filter(id=home['location_id']).values('address','latitude','longitude')
+        home['location'] = location[0]
     return JsonResponse({'agency': agency[0], 'homes':homes_list}, safe=False)
 
 
