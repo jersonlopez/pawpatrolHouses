ac772feda03953e28f80b17b830b8a1c3be1a4bc
dugol <danielugiraldo@gmail.com>
2018-05-05 10:11:34 -0500

    casas

--- homes/urls.py
+++ homes/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('v1/homes/search/', views.getAllHomes, name='homes'),
+    path('v1/homes/search', views.getAllHomes, name='homes'),
     path('v1/homes/<searchCity>', views.getHomesByCity, name='homes'),
 ]
