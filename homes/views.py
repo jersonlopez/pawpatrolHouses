6800940e53dd30b6e6652c0811dc5f92b72faed3
sonorks <julivas96@gmail.com>
2018-05-05 04:19:06 -0500

    add Agency to Response

--- homes/views.py
+++ homes/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render
 from django.core import serializers
 from django.http import HttpResponse, JsonResponse
 
-from .models import Homes
+from .models import Homes, Agency
 
 # Create your views here.
 def getAllHomes(request):
+    agency = Agency.objects.values('name','nit')
     homes = Homes.objects.all().values()
     homes_list = list(homes)
-    return JsonResponse(homes_list, safe=False)
+    return JsonResponse({'agency': agency[0], 'homes':homes_list}, safe=False)
 
 
 def getHomesByCity(request, searchCity):
