50c2bea06555316ccabd33456c46891b9fae0166
jersonlopez <jersonlopez.1204@gmail.com>
2018-06-01 04:20:54 -0500

    decoding firebade token

--- homes/urls.py
+++ homes/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('v1/homes/<searchCity>', views.getHomesByCity, name='searchHomes'),
     path('v1/homes/booking/', views.addBooking, name="booking"),
     path('v1/homes/myBooking/', views.getBookingsByUser, name = "getBooking"),
+    path('v1/homes/try/', verify, name = "try"),
 ]
